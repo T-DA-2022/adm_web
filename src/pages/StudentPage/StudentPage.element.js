d6fae823a6537119192a46627b4a44f092da879b
moong <moonki0623@naver.com>
2023-01-27 03:56:06 +0900

    student page modify

--- src/pages/StudentPage/StudentPage.element.js
+++ src/pages/StudentPage/StudentPage.element.js
@@ -5,6 +5,7 @@ export const StudentPageContainer = styled.div`
   width: ${(props) => `${props.width}px`};
   height: calc(100vh - 60px);
   display: flex;
+  flex-direction: column;
 `;
 
 export const GraphFlex = styled.div`
@@ -18,4 +19,32 @@ export const GraphContainer = styled.div`
       ? `calc(${props.width}px / 2 + 80px)`
       : `calc(${props.width}px / 2 - 0px)`};
   height: 220px;
+`;
+
+export const GraphBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  border: 1px solid black;
+  width: 100%;
+  height: 300px;
+`;
+
+export const SpecificGraphContainer = styled.div`
+  width: 100%;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SpecificRadioDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const SpecificGraphBox = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
 `;
--- src/pages/StudentPage/StudentPage.js
+++ src/pages/StudentPage/StudentPage.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
+  GraphBox,
   GraphContainer,
   GraphFlex,
+  SpecificGraphBox,
+  SpecificGraphContainer,
+  SpecificRadioDiv,
   StudentPageContainer,
 } from "./StudentPage.element";
 
+import CustomModal from "../../components/CustomModal/CustomModal";
+import TwoDayCompare from "../../components/CompareBox/TwoDayCompare";
+import PeriodCompare from "../../components/CompareBox/PeriodCompare";
+import ProCompare from "../../components/CompareBox/ProCompare";
 import MyResponsiveRadar from "../../components/CustomRadar/CustomRadar";
 
 import { AGraphData, BGraphData } from "../../dummy/GraphDummy";
+
 const StudentPage = ({ width }) => {
+  const [student, setStudent] = useState([]);
   const legend_data = [
     {
       data: [
@@ -42,21 +52,57 @@ const StudentPage = ({ width }) => {
 
   return (
     <StudentPageContainer width={width}>
-      <GraphFlex>
-        <GraphContainer legend={true} width={width}>
-          <MyResponsiveRadar
-            data={AGraphData}
-            keys={["last_week", "this_week", "pro_debut"]}
-            legend_data={legend_data}
+      <GraphBox>
+        <GraphFlex>
+          <GraphContainer legend={true} width={width}>
+            <MyResponsiveRadar
+              data={AGraphData}
+              keys={["last_week", "this_week", "pro_debut"]}
+              legend_data={legend_data}
+            />
+          </GraphContainer>
+          <GraphContainer legend={false} width={width}>
+            <MyResponsiveRadar
+              data={BGraphData}
+              keys={["last_week", "this_week", "pro_debut"]}
+            />
+          </GraphContainer>
+        </GraphFlex>
+      </GraphBox>
+      <SpecificGraphContainer>
+        <SpecificRadioDiv>
+          <CustomModal
+            selected={student}
+            selectList={[
+              {
+                id: 1,
+                name: "선택비교",
+              },
+              {
+                id: 2,
+                name: "기간비교",
+              },
+              {
+                id: 3,
+                name: "프로비교",
+              },
+            ]}
+            setStudent={setStudent}
+            optionWord="옵션"
           />
-        </GraphContainer>
-        <GraphContainer legend={false} width={width}>
-          <MyResponsiveRadar
-            data={BGraphData}
-            keys={["last_week", "this_week", "pro_debut"]}
-          />
-        </GraphContainer>
-      </GraphFlex>
+        </SpecificRadioDiv>
+        <SpecificGraphBox>
+          {student.name === "선택비교" ? (
+            TwoDayCompare()
+          ) : student.name === "기간비교" ? (
+            PeriodCompare()
+          ) : student.name === "프로비교" ? (
+            ProCompare()
+          ) : (
+            <div>옵션을 선택해주세요</div>
+          )}
+        </SpecificGraphBox>
+      </SpecificGraphContainer>
     </StudentPageContainer>
   );
 };
