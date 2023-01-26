cfe96dffe8134088a3d741009e7dd36d724f92a6
moong <moonki0623@naver.com>
2023-01-27 03:55:21 +0900

    css for all the comparebox cpnt

---
+++ src/components/CompareBox/CompareBox.element.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const GraphOptionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+export const GraphContainer = styled.div`
+  width: 100%;
+  min-height: 300px;
+`;
+
+export const CompareDivContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
