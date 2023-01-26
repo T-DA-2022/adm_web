6abdda9974f92ed066e0c5e902ae6d8b74d522cb
moong <moonki0623@naver.com>
2023-01-27 03:55:58 +0900

    modify modal component

--- src/components/CustomModal/CustomModal.js
+++ src/components/CustomModal/CustomModal.js
@@ -2,7 +2,7 @@ import { Button, Modal } from "antd";
 import { useState } from "react";
 import { ModalP } from "./CustomModal.element";
 
-const CustomModal = ({ selected, selectList, setStudent }) => {
+const CustomModal = ({ selected, selectList, setStudent, optionWord }) => {
   //   console.log(selected);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalitem, setModalItem] = useState(selected);
@@ -15,6 +15,7 @@ const CustomModal = ({ selected, selectList, setStudent }) => {
   };
   const handleCancel = () => {
     setIsModalOpen(false);
+    setModalItem(selected);
   };
   const clickHandler = (item) => {
     console.log(item);
@@ -31,10 +32,10 @@ const CustomModal = ({ selected, selectList, setStudent }) => {
   return (
     <>
       <Button type="default" size="large" onClick={showModal}>
-        {selected.name} 학생
+        {selected.name} {optionWord}
       </Button>
       <Modal
-        title={`학생 선택 : ${modalitem.name}`}
+        title={`${optionWord} 선택 : ${modalitem.name}`}
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
